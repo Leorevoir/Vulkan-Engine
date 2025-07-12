842c21766b99bd7c5a1fc38da67415c0c6ce6130
Leorevoir <leo.quinzler@epitech.eu>
2025-07-12 11:55:21 +0200

    rename: VKE -> VK

---
+++ src/VK/Window/Window.cpp
@@ -0,0 +1,19 @@
+// #include <VK/Error.hpp>
+// #include <VK/Window/Window.hpp>
+//
+// /**
+// * public
+// */
+//
+// vk::Window::Window(const maths::Vector2u &size, const std::string &title)
+// {
+//     /* __ctor__ */
+//     if (!VK_MATHS_NOT_NULL(size.x) && !VK_MATHS_NOT_NULL(size.y)) {
+//         throw vk::exception::InvalidArgument("vk::Window::Window", "Window size must be > 0");
+//     }
+// }
+//
+// vk::Window::~Window()
+// {
+//     /* __dtor__ */
+// }
