54ca4cef4f39b753d3ad0e377efb3049a3922639
Leorevoir <leo.quinzler@epitech.eu>
2025-07-13 07:54:17 +0200

    feat: better building script with rules

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ RED="\033[1;31m"
 ILC="\033[3m"
 ORG="\033[1;33m"
 RST="\033[0m"
-BOLD="\033[1m"
+
+PROGRAM_NAME="vulkan_engine"
 
 function _error()
 {
@@ -15,61 +16,153 @@ function _error()
 
 function _success()
 {
-    echo -e "${GREEN}[✅] SUCCESS:\t${RST}${ILC}$1${RST}"
+    echo -e "${GREEN}[✅] SUCCESS:\t${RST} ${ILC}$1${RST}"
 }
 
 function _info()
 {
-    echo -e "${ORG}[🚧] RUNNING:\t${RST}${ILC}$1${RST}"
+    echo -e "${ORG}[🚧] RUNNING:\t${RST} ${ILC}$1${RST}"
 }
 
-function _check_dep()
+function _run_compression()
 {
-    local dep="$1"
-    command -v "$dep" &>/dev/null || _error "Missing dependency" "$dep is not installed or not in PATH"
+    if ! { command -v python3 > /dev/null; } 2>&1; then
+        _error "command 'python3' not found" "please install 'python3' or 'nix develop' 🤓"
+    fi
+    _info "command 'python3' found, running extraction script..."
+    if ! python3 script/compression.py unzip assets/models/models.zip assets/models; then
+        _error "script/compression.py error" "failed to run extraction script for assets/models"
+    fi
+    if ! python3 script/compression.py unzip assets/textures/textures.zip assets/textures; then
+        _error "script/compression.py error" "failed to run extraction script for assets/textures"
+    fi
+    _success "extraction script completed successfully"
 }
 
-function _check_pkg()
+function _build_shader_cache()
 {
-    pkg-config --exists "$1" || _error "Missing package" "$1 (pkg-config could not find it)"
+    _info "compiling shaders to SPIR-V..."
+    find ./assets/shaders/ -type f \( -name "*.frag" -o -name "*.vert" -o -name "*.comp" \) \
+    | parallel --eta '
+        ext="{= s:.*\.(frag|vert)$:\1: =}";
+        base=$(basename {} .$ext);
+        out=${base}_${ext}.spv;
+        echo "Compiling {} -> $out";
+        glslangValidator -V {} -o $out
+    '
+    _success "shaders compiled to SPIR-V !"
 }
 
-_info "Checking build dependencies..."
+function _check_cmd()
+{
+    if ! { command -v $1 > /dev/null; } 2>&1; then
+        _error "command '$1' not found" "please install '$1' or 'nix develop' 🤓"
+    fi
+    _success "command '$1' found"
+}
 
-for bin in cmake ninja pkg-config glslangValidator; do
-    _check_dep "$bin"
+function _base_run()
+{
+    local cmake_args="$1"
+
+    _check_cmd cmake
+    _check_cmd ninja
+    _check_cmd glslangValidator
+
+    _info "updating external submodules..."
+    git submodule update --init --recursive
+    _success "updated external submodules !"
+    _build_shader_cache
+    mkdir -p build
+    cd build || _error "mkdir failed"
+
+    cmake .. -G Ninja $cmake_args
+    if ! ninja $PROGRAM_NAME; then
+        _error "compilation error" "failed to compile $PROGRAM_NAME"
+    fi
+    _success "compiled $PROGRAM_NAME"
+}
+
+function _all()
+{
+    _base_run "-DCMAKE_BUILD_TYPE=Release -DENABLE_DEBUG=OFF"
+    exit 0
+}
+
+function _debug()
+{
+    _base_run "-DCMAKE_BUILD_TYPE=Debug -DENABLE_DEBUG=ON"
+    exit 0
+}
+
+function _tests_run()
+{
+    _base_run "-DCMAKE_BUILD_TYPE=Debug -DENABLE_DEBUG=ON"
+    cd .. || _error "cd failed"
+    if ! ./unit_tests; then
+        _error "unit tests error" "unit tests failed!"
+    fi
+    _success "unit tests succeed!"
+    if [ "$(uname -s)" == 'Darwin' ]; then
+        xcrun llvm-profdata merge -sparse unit_tests-*.profraw -o unit_tests.profdata
+        xcrun llvm-cov report ./unit_tests -instr-profile=unit_tests.profdata -ignore-filename-regex='.*/tests/.*' -enable-name-compression > code_coverage.txt
+    else
+        gcovr -r . --exclude tests/ > code_coverage.txt
+    fi
+    cat code_coverage.txt
+    exit 0
+}
+
+function _clean()
+{
+    rm -rf build
+}
+
+function _fclean()
+{
+    _clean
+    rm -rf $PROGRAM_NAME unit_tests plugins code_coverage.txt unit_tests-*.profraw unit_tests.profdata vgcore* cmake-build-debug
+}
+
+for args in "$@"
+do
+    case $args in
+        -h|--help)
+            cat << EOF
+USAGE:
+      $0    builds $PROGRAM_NAME project
+
+ARGUMENTS:
+      $0 [-h|--help]    displays this message
+      $0 [-d|--debug]   debug flags compilation
+      $0 [-c|--clean]   clean the project
+      $0 [-f|--fclean]  fclean the project
+      $0 [-t|--tests]   run unit tests
+EOF
+        exit 0
+        ;;
+    -c|--clean)
+        _clean
+        exit 0
+        ;;
+    -f|--fclean)
+        _fclean
+        exit 0
+        ;;
+    -d|--debug)
+        _debug
+        ;;
+    -t|--tests)
+        _tests_run
+        ;;
+    -r|--re)
+        _fclean
+        _all
+        ;;
+    *)
+        _error "Invalid arguments: " "$args"
+    esac
 done
 
-_check_pkg "vulkan"
+_all
 
-_success "All required tools and libraries are available"
-
-_info "Updating git submodules..."
-git submodule update -j16 --init --recursive
-_success "Git submodules updated successfully"
-
-_info "Compiling shaders to SPIR-V..."
-find ./assets/shaders/ -type f \( -name "*.frag" -o -name "*.vert" -o -name "*.comp" \) \
-| parallel --eta '
-    ext="{= s:.*\.(frag|vert)$:\1: =}";
-    base=$(basename {} .$ext);
-    out=${base}_${ext}.spv;
-    echo "Compiling {} -> $out";
-    glslangValidator -V {} -o $out
-'
-_success "Built shader cache"
-
-BUILD_DIR="build"
-
-_info "Creating build directory \"$BUILD_DIR\"..."
-mkdir -p "$BUILD_DIR" || _error "Failed to create build directory" "$BUILD_DIR"
-
-cd "$BUILD_DIR" || _error "Failed to enter build directory" "$BUILD_DIR"
-
-_info "Configuring CMake project with Ninja generator..."
-cmake -G Ninja .. || _error "CMake configuration failed" "Check CMakeLists.txt or missing dependencies"
-
-_info "Building project with Ninja..."
-ninja || _error "Build failed" "Fix compilation errors above ⬆"
-
-_success "Build completed successfully"
