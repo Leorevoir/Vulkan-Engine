2f1a181cec539a50012f5fe1d2e142047e4b0483
Leorevoir <leo.quinzler@epitech.eu>
2025-07-23 19:06:47 +0200

    add: third person camera with smoothing

--- src/VKE/Scene/Camera/ThirdPersonCamera.cpp
+++ src/VKE/Scene/Camera/ThirdPersonCamera.cpp
@@ -1,5 +1,14 @@
+#include <VKE/Core/Time.hpp>
+#include <VKE/Maths/Maths.hpp>
 #include <VKE/Scene/Camera/ThirdPersonCamera.hpp>
 #include <VKE/Systems/MouseEvent.hpp>
+
+/** TODO: make parameters / setters configurable */
+#define TPC_ROTATION_SENSITIVITY 150.f
+#define TPC_ROTATION_SMOOTHING 8.f
+#define TPC_ZOOM_SENSITIVITY 2.f
+#define TPC_ZOOM_SMOOTHING 5.f
+#define TPC_MAX_VERTICAL_ANGLE 89.f
 
 /**
  * protected
@@ -7,34 +16,57 @@
 
 void vke::ThirdPersonCamera::updateThirdPersonCamera()
 {
+    const f32 delta_time = core::Time::getInstance().getDeltaTime();
+
+    _third_person_camera_rotation(delta_time);
+    _third_person_camera_interpolation(delta_time);
+    _third_person_camera_zoom(delta_time);
+}
+
+/**
+* private
+*/
+
+void vke::ThirdPersonCamera::_third_person_camera_rotation(const f32 dt)
+{
     const auto &mouse_event = vke::event::MouseEvent::getInstance();
+
+    if (!mouse_event.getButton().left) {
+        return;
+    }
+
     const auto &mouse_position = mouse_event.getPosition();
+    const float delta_x = mouse_position.x - _old_position.x;
+    const float delta_y = mouse_position.y - _old_position.y;
+
+    _target_rotation.y += delta_x * TPC_ROTATION_SENSITIVITY * dt;
+    _target_rotation.x += delta_y * TPC_ROTATION_SENSITIVITY * dt;
+
+    _target_rotation.x = maths::clamp(_target_rotation.x, -TPC_MAX_VERTICAL_ANGLE, TPC_MAX_VERTICAL_ANGLE);
+}
+
+void vke::ThirdPersonCamera::_third_person_camera_interpolation(const f32 dt)
+{
+    _camera_rotation.x = maths::lerp(_camera_rotation.x, _target_rotation.x, TPC_ROTATION_SMOOTHING * dt);
+    _camera_rotation.y = maths::lerp(_camera_rotation.y, _target_rotation.y, TPC_ROTATION_SMOOTHING * dt);
+}
+
+void vke::ThirdPersonCamera::_third_person_camera_zoom(const f32 dt)
+{
+    const auto &mouse_event = vke::event::MouseEvent::getInstance();
     const auto &mouse_scroll = mouse_event.getScroll();
+    f32 zoom_input = 0.f;
 
-    /** rotation */
-    if (mouse_event.getButton().left) {
-
-        if (mouse_position.x > _old_position.x) {
-            _camera_rotation.y += 1.f;
-        } else if (mouse_position.x < _old_position.x) {
-            _camera_rotation.y -= 1.f;
-        }
-        if (mouse_position.y > _old_position.y) {
-            _camera_rotation.x += 1.f;
-        } else if (mouse_position.y < _old_position.y) {
-            _camera_rotation.x -= 1.f;
-        }
-    }
-
-    /** zoom */
     if (mouse_scroll.up) {
-        _camera_distance += 0.1f;
+        zoom_input = TPC_ZOOM_SENSITIVITY * dt;
     } else if (mouse_scroll.down) {
-        _camera_distance -= 0.1f;
+        zoom_input = -TPC_ZOOM_SENSITIVITY * dt;
     }
 
-    _camera_zoom += _camera_distance;
-    _camera_distance = 0.f;
+    _target_zoom += zoom_input;
+    _target_zoom = maths::clamp(_target_zoom, 1.f, 50.f);
 
-    _old_position = mouse_position;
+    /** interpolate zoom */
+    _camera_zoom = maths::lerp(_camera_zoom, _target_zoom, TPC_ZOOM_SMOOTHING * dt);
+    _old_position = mouse_event.getPosition();
 }
--- src/VKE/Scene/Camera/ThirdPersonCamera.hpp
+++ src/VKE/Scene/Camera/ThirdPersonCamera.hpp
@@ -17,11 +17,17 @@ class VKE_API ThirdPersonCamera
 
         maths::Vector3f _camera_position = {};
         maths::Vector3f _camera_rotation = {};
+        maths::Vector3f _target_rotation = {};
+        f32 _target_zoom = 5.f;
         f32 _camera_zoom = -2.f;
         f32 _camera_distance = 5.f;
 
     private:
         event::MousePosition _old_position = {};
+
+        void _third_person_camera_rotation(const f32 dt);
+        void _third_person_camera_interpolation(const f32 dt);
+        void _third_person_camera_zoom(const f32 dt);
 };
 
 }// namespace vke
