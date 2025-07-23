7dcb1ed8c419a4e0488bc697e7fb74fc53c12b29
Leorevoir <leo.quinzler@epitech.eu>
2025-07-23 19:06:34 +0200

    add: clamp + lerp methods

--- src/VKE/Maths/Maths.hpp
+++ src/VKE/Maths/Maths.hpp
@@ -11,5 +11,17 @@ static inline constexpr VKEM_API f32 radians(const f32 degrees)
     return VKEM_DEG2RAD(degrees);
 }
 
+template<typename T>
+static inline constexpr VKEM_API T lerp(const T a, T b, const T t)
+{
+    return a + t * (b - a);
+}
+
+template<typename T>
+static inline constexpr VKEM_API T clamp(const T value, const T min_value, const T max_value)
+{
+    return (value < min_value) ? min_value : ((value > max_value) ? max_value : value);
+}
+
 }// namespace maths
 }// namespace vke
