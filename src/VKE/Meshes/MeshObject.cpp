312301f10a222ab9c2c2d6e02cefe18349657016
Leorevoir <leo.quinzler@epitech.eu>
2025-07-25 00:42:52 +0200

    move: shader&uniform logic to mesh

--- src/VKE/Meshes/MeshObject.cpp
+++ src/VKE/Meshes/MeshObject.cpp
@@ -1,6 +1,9 @@
 #include <VKE/Error.hpp>
 #include <VKE/Meshes/MeshObject.hpp>
 
+#include <VKE/Graphics/Device/VulkanBuffer.hpp>
+#include <VKE/Graphics/Shader/VulkanShader.hpp>
+
 /**
 * public
 */
@@ -9,22 +12,34 @@ vke::MeshObject::~MeshObject()
 {
     _vertex_buffer.destroy();
     _index_buffer.destroy();
+    _color_buffer.destroy();
 }
 
 void vke::MeshObject::bind(VkCommandBuffer &cmd_buffer, std::shared_ptr<VulkanShader> vulkan_shader)
 {
-    VkDeviceSize offsets[1] = {0};
-    auto pipeline = vulkan_shader->getPipeline();
+    const auto pipeline = vulkan_shader->getPipeline();
 
-    if (pipeline) {
-        vkCmdBindPipeline(cmd_buffer, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline);
-    } else {
+    if (!pipeline) {
         throw exception::RuntimeError("MeshObject::build", "Pipeline is not set for the shader.");
     }
 
-    vkCmdBindVertexBuffers(cmd_buffer, 0, 1, &_vertex_buffer._buffer, offsets);
+    vkCmdBindPipeline(cmd_buffer, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline);
+
+    const VkBuffer vertex_buffers[] = {_vertex_buffer._buffer, _color_buffer._buffer};
+    const VkDeviceSize offsets[] = {0, 0};
+
+    vkCmdBindVertexBuffers(cmd_buffer, 0, 2, vertex_buffers, offsets);
     vkCmdBindIndexBuffer(cmd_buffer, _index_buffer._buffer, 0, VK_INDEX_TYPE_UINT32);
     vkCmdDrawIndexed(cmd_buffer, _index_count, 1, 0, 0, 0);
+}
+
+void vke::MeshObject::bind(VkCommandBuffer &cmd_buffer)
+{
+    if (!_shader) {
+        throw exception::RuntimeError("MeshObject::bind", "Shader is not set for the MeshObject.");
+    }
+
+    this->bind(cmd_buffer, _shader);
 }
 
 /**
@@ -63,6 +78,11 @@ void vke::MeshObject::setRotation(const vke::maths::Vector3f &rotation)
 void vke::MeshObject::setScale(const vke::maths::Vector3f &scale)
 {
     _scale = scale;
+}
+
+void vke::MeshObject::setColor(const vke::maths::Vector4f &color)
+{
+    _color = color;
 }
 
 /**
--- src/VKE/Meshes/MeshObject.hpp
+++ src/VKE/Meshes/MeshObject.hpp
@@ -5,12 +5,13 @@
 #include <VKE/Macros.hpp>
 #include <VKE/Maths/Vector.hpp>
 
-#include <VKE/Graphics/Device/VulkanBuffer.hpp>
-#include <VKE/Graphics/Shader/VulkanShader.hpp>
-
 #include <memory>
 
 namespace vke {
+
+class VulkanShader;
+class VulkanBuffer;
+class UniformCamera;
 
 class VKE_API MeshObject : public VulkanObject
 {
@@ -21,6 +22,7 @@ class VKE_API MeshObject : public VulkanObject
         /* + initialize() */
         /* + update() */
         void bind(VkCommandBuffer &cmd_buffer, std::shared_ptr<VulkanShader> vulkan_shader);
+        void bind(VkCommandBuffer &cmd_buffer);
 
         /** getters */
         const maths::Vector3f &getPosition() const;
@@ -31,16 +33,22 @@ class VKE_API MeshObject : public VulkanObject
         void setPosition(const maths::Vector3f &position);
         void setRotation(const maths::Vector3f &rotation);
         void setScale(const maths::Vector3f &scale);
+        void setColor(const maths::Vector4f &color);
 
     protected:
         maths::Vector3f _position = {};
         maths::Vector3f _rotation = {};
         maths::Vector3f _scale = {1.0f, 1.0f, 1.0f};
+        maths::Color _color = {0.f, 1.f, 0.f, 1.f};
 
         u32 _index_count = 0;
 
         VulkanBuffer _vertex_buffer;
         VulkanBuffer _index_buffer;
+        VulkanBuffer _color_buffer;
+
+        std::shared_ptr<UniformCamera> _uniform = VKE_NULLPTR;
+        std::shared_ptr<VulkanShader> _shader = VKE_NULLPTR;
 };
 
 }// namespace vke
