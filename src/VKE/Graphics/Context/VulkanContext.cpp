663766ebb75469c491f2182699a88c466b067b11
Leorevoir <leo.quinzler@epitech.eu>
2025-07-25 00:44:03 +0200

    feat: better VulkanContext

--- src/VKE/Graphics/Context/VulkanContext.cpp
+++ src/VKE/Graphics/Context/VulkanContext.cpp
@@ -9,7 +9,30 @@ vke::VulkanContext::VulkanContext(priv::VulkanDevice *device, maths::Vector2u *s
     /* __ctor__ */
 }
 
-VkDevice &vke::VulkanContext::getDevice()
+/**
+ * setters
+ */
+
+void vke::VulkanContext::setPipelines(priv::VulkanPipelines *pipelines)
+{
+    _pipelines = pipelines;
+}
+
+void vke::VulkanContext::setRenderPass(VkRenderPass *render_pass)
+{
+    _render_pass = render_pass;
+}
+
+void vke::VulkanContext::setDescriptorSet(priv::VulkanDescriptorSet *descriptor_set)
+{
+    _descriptor_set = descriptor_set;
+}
+
+/**
+ * getters
+*/
+
+VkDevice &vke::VulkanContext::getLogicalDevice()
 {
     return _device->_logicalDevice;
 }
@@ -17,4 +40,29 @@ VkDevice &vke::VulkanContext::getDevice()
 VkPhysicalDevice &vke::VulkanContext::getPhysicalDevice()
 {
     return _device->_physicalDevice;
+}
+
+VkRenderPass *vke::VulkanContext::getRenderPass()
+{
+    return _render_pass;
+}
+
+vke::priv::VulkanDevice *vke::VulkanContext::getDevice()
+{
+    return _device;
+}
+
+vke::priv::VulkanPipelines *vke::VulkanContext::getPipelines()
+{
+    return _pipelines;
+}
+
+vke::maths::Vector2u *vke::VulkanContext::getScreenSize()
+{
+    return _screen_size;
+}
+
+vke::priv::VulkanDescriptorSet *vke::VulkanContext::getDescriptorSet()
+{
+    return _descriptor_set;
 }
