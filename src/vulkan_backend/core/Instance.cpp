07134719fe86b170c32ac2aaff1d52e753359e69
Leorevoir <leo.quinzler@epitech.eu>
2025-11-09 17:29:20 +0100

    fix: vulkan api version

--- src/vulkan_backend/core/Instance.cpp
+++ src/vulkan_backend/core/Instance.cpp
@@ -87,7 +87,7 @@ void lumen::Instance::_create_instance(const char *appName)
     appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.pEngineName = "Lumen";
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.apiVersion = VK_API_VERSION_1_0;
+    appInfo.apiVersion = VK_API_VERSION_1_4;
 
     VkInstanceCreateInfo createInfo{};
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
