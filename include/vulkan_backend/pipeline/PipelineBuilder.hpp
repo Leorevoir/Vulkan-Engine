5c436acc1990b17811bb7b3cd49358cabc736bee
Leorevoir <leo.quinzler@epitech.eu>
2025-11-09 17:48:08 +0100

    feat: modular code for the pipeline

--- include/vulkan_backend/pipeline/PipelineBuilder.hpp
+++ include/vulkan_backend/pipeline/PipelineBuilder.hpp
@@ -1,24 +1,48 @@
 #pragma once
 
+#include <memory>
+#include <string>
 #include <vector>
 #include <vulkan/vulkan.h>
 
 namespace lumen {
 
 class Device;
+class Pipeline;
+class Shader;
 
 class PipelineBuilder
 {
     public:
-        PipelineBuilder(Device &device);
-        ~PipelineBuilder() = default;
+        explicit PipelineBuilder(Device &device) noexcept;
+        ~PipelineBuilder() noexcept;
 
-        // TODO: Add methods to configure vertex input, shaders, rasterizer, etc.
+        PipelineBuilder &set_shader_stages(const std::string &vert_path, const std::string &frag_path);
+        PipelineBuilder &set_vertex_input_state() noexcept;
+        PipelineBuilder &set_input_assembly_state() noexcept;
+        PipelineBuilder &set_viewport_state() noexcept;
+        PipelineBuilder &set_rasterization_state() noexcept;
+        PipelineBuilder &set_multisample_state() noexcept;
+        PipelineBuilder &set_color_blend_state() noexcept;
+        PipelineBuilder &set_dynamic_state(const std::vector<VkDynamicState> &states) noexcept;
 
-        VkPipeline build(VkRenderPass renderPass, VkPipelineLayout layout);
+        VkPipeline build(VkRenderPass render_pass, VkPipelineLayout *out_pipeline_layout);
 
     private:
         Device &_device;
+
+        VkPipelineVertexInputStateCreateInfo _vertexInputInfo{};
+        VkPipelineInputAssemblyStateCreateInfo _inputAssemblyInfo{};
+        VkPipelineViewportStateCreateInfo _viewportInfo{};
+        VkPipelineRasterizationStateCreateInfo _rasterizationInfo{};
+        VkPipelineMultisampleStateCreateInfo _multisampleInfo{};
+        VkPipelineColorBlendAttachmentState _colorBlendAttachment{};
+        VkPipelineColorBlendStateCreateInfo _colorBlendInfo{};
+        VkPipelineDynamicStateCreateInfo _dynamicStateInfo{};
+
+        std::vector<VkDynamicState> _dynamicStateEnables;
+        std::vector<VkPipelineShaderStageCreateInfo> _shaderStages;
+        std::vector<std::unique_ptr<Shader>> _shaderModules;
 };
 
 }// namespace lumen
