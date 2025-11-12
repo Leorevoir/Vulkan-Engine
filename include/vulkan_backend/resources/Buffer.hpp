5a1ea73bddb1d553a5f542f0eb3259e71e57ac52
Leorevoir <leo.quinzler@epitech.eu>
2025-11-12 11:03:42 +0100

    add: vulkan buffer header

--- include/vulkan_backend/resources/Buffer.hpp
+++ include/vulkan_backend/resources/Buffer.hpp
@@ -1,19 +1,26 @@
 #pragma once
 
-#include <vulkan_backend/Backend.hpp>
+#include <vulkan_backend/core/VulkanObject.hpp>
+#include <vulkan_backend/memory/Allocator.hpp>
 
 namespace lumen {
 
-class Buffer
+class GraphicsContext;
+
+class Buffer : public VulkanObject<VkBuffer>
 {
     public:
-        Buffer();
-        ~Buffer();
+        Buffer(GraphicsContext &context, VkDeviceSize size, VkBufferUsageFlags buffer_usage, VkMemoryPropertyFlags memory_properties);
+        ~Buffer() override;
 
-        VkBuffer handle() const;
+        void write(const void *data, VkDeviceSize size);
+        void *map();
+        void unmap();
 
     private:
-        VkBuffer _buffer = VK_NULL_HANDLE;
+        GraphicsContext &_context;
+        Allocation _allocation;
+        VkDeviceSize _size;
 };
 
 }// namespace lumen
