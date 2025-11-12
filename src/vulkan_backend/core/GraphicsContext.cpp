0dd18eb27c2bba140a5888097bb132c97b9d9619
Leorevoir <leo.quinzler@epitech.eu>
2025-11-12 13:24:32 +0100

    add: Memory Allocator to vulkan graphics context

--- src/vulkan_backend/core/GraphicsContext.cpp
+++ src/vulkan_backend/core/GraphicsContext.cpp
@@ -10,7 +10,7 @@ lumen::GraphicsContext::GraphicsContext(RenderWindow &window)
     _init_vulkan(window);
 }
 
-lumen::GraphicsContext::~GraphicsContext()
+lumen::GraphicsContext::~GraphicsContext() noexcept
 {
     /* __dtor__ */
 }
@@ -19,24 +19,29 @@ lumen::GraphicsContext::~GraphicsContext()
  * getters
  */
 
-lumen::Instance &lumen::GraphicsContext::get_instance() const
+lumen::Instance &lumen::GraphicsContext::get_instance() const noexcept
 {
     return *_instance;
 }
 
-lumen::Device &lumen::GraphicsContext::get_device() const
+lumen::Device &lumen::GraphicsContext::get_device() const noexcept
 {
     return *_device;
 }
 
-lumen::Surface &lumen::GraphicsContext::get_surface() const
+lumen::Surface &lumen::GraphicsContext::get_surface() const noexcept
 {
     return *_surface;
 }
 
-lumen::CommandPool &lumen::GraphicsContext::get_command_pool() const
+lumen::CommandPool &lumen::GraphicsContext::get_command_pool() const noexcept
 {
     return *_commandPool;
+}
+
+lumen::Allocator &lumen::GraphicsContext::get_allocator() const noexcept
+{
+    return *_allocator;
 }
 
 /**
@@ -48,5 +53,6 @@ void lumen::GraphicsContext::_init_vulkan(RenderWindow &window)
     _instance = std::make_unique<Instance>("Lumen App");
     _surface = std::make_unique<Surface>(*_instance, window.get_native_handle());
     _device = std::make_unique<Device>(*_instance, *_surface);
+    _allocator = std::make_unique<Allocator>(*_device);
     _commandPool = std::make_unique<CommandPool>(*_device, _device->getQueueFamilyIndices().graphicsFamily.value());
 }
