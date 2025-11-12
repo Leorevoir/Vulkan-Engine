42e95ed7311def5a937202357a8b4221dcae9179
Leorevoir <leo.quinzler@epitech.eu>
2025-11-12 10:59:45 +0100

    add: vulkan allocator & memory pool headers

--- include/vulkan_backend/memory/MemoryPool.hpp
+++ include/vulkan_backend/memory/MemoryPool.hpp
@@ -1,12 +1,23 @@
 #pragma once
 
+#include <vulkan_backend/Backend.hpp>
+#include <vulkan_backend/core/VulkanObject.hpp>
+
 namespace lumen {
 
-class MemoryPool
+class MemoryPool : public VulkanObject<VkDeviceMemory>
 {
     public:
-        MemoryPool() = default;
-        ~MemoryPool() = default;
+        MemoryPool(Device &device, const uint32_t memoryTypeIndex, const VkDeviceSize size);
+        ~MemoryPool();
+
+        VkDeviceSize allocate(const VkDeviceSize size, const VkDeviceSize alignment);
+
+        static constexpr VkDeviceSize InvalidAllocation = static_cast<VkDeviceSize>(-1);
+
+    private:
+        VkDeviceSize _size = 0;
+        VkDeviceSize _offset = 0;
 };
 
 }// namespace lumen
